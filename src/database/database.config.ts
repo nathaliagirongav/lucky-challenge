cc550ef81e680bed88d5127564acf7e8c63d4d7c
Nathalia Girón <nathalia@MacBook-Pro-de-Nathalia.local>
2022-12-26 00:17:42 -0500

    Fix when getting env variables

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -12,12 +12,12 @@ export class DatabaseConfig {
   public getConnectionConfig(): DataSourceOptions {
     return {
       type: 'mysql',
-      timezone: this.envConfig['APP_TIMEZONE'],
-      host: this.envConfig['DATABASE_HOST'] || 'lucky-challenge-mysql',
-      username: this.envConfig['DATABASE_USERNAME'] || 'root',
-      password: this.envConfig['DATABASE_PASSWORD'] || 'root',
-      database: this.envConfig['DATABASE_NAME'] || 'lucky-challenge',
-      port: Number(this.envConfig['DATABASE_PORT']) || 2409,
+      timezone: `${this.envConfig['APP_TIMEZONE']}`,
+      host: `${this.envConfig['DATABASE_HOST']}` || 'lucky-challenge-mysql',
+      username: `${this.envConfig['DATABASE_USERNAME']}` || 'root',
+      password: `${this.envConfig['DATABASE_PASSWORD']}` || 'root',
+      database: `${this.envConfig['DATABASE_NAME']}` || 'lucky-challenge',
+      port: Number(`${this.envConfig['DATABASE_PORT']}`) || 2409,
       synchronize: false,
       migrationsRun: false,
     };
